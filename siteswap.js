6b94ede6784d325d71296f753ae7097d2668bdb0
HuxleyBerry <huxley.berry@gmail.com>
2023-08-07 14:16:51 +0800

    add options for outside throws

--- siteswap.js
+++ siteswap.js
@@ -5,6 +5,7 @@ let ctx = canvas.getContext("2d");
 let gravitySlider = document.getElementById("gravity-slider");
 let beatSlider = document.getElementById("beat-slider");
 let siteswapButton = document.getElementById("siteswap-button");
+let showTwosCheckbox = document.getElementById("show-twos");
 
 let animationOngoing = false;
 let startingTime;
@@ -14,11 +15,14 @@ let globalSync;
 
 let beatLength; // length in milliseconds
 let gravity;
+let showTwosAsHolds = false;
+let LHoutsideThrows = false;
+let RHoutsideThrows = false;
 
 const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
-const asyncExamples = ["534","12345","441","3","4","5","6","7","744","633","1357","51","17","53","423","525","50505","5551","7131","561","4453","612","73","312","531","61616","663","5241","5313","5524","7333","7571","45141","52512","56414"];
-const syncExamples = ["(2x,4x)","(4,2x)(2x,4)","(4,4)","(4,4)(4x,0)(4,4)(0,4x)","(4,6x)(2x,4)","(4x,2x)","(4x,2x)(4,2x)(2x,4x)(2x,4)","(4x,4x)","(4x,6)(6,4x)","(6,4x)(4x,2)","(6x,2x)","(6x,2x)(2x,6x)","(6x,4)(4,2x)(4,6x)(2x,4)","(6x,4)(4,6x)","(6x,4x)","(6x,6x)(2x,2x)","(2x,2x)","(8,2x)(4,2x)(2x,8)(2x,4)"];
-const multiplexExamples = ["[54]24","[43]1421","4[43]1","[32]","[43]23","[43][32]3","[31]","(2,4)([4x4],2x)","(2,4x)([4x4],2)","(2,6)([6x6],2x)(6x,2x)"];
+const asyncExamples = ["534", "12345", "441", "3", "4", "5", "6", "7", "744", "633", "1357", "51", "17", "53", "423", "525", "50505", "5551", "7131", "561", "4453", "612", "73", "312", "531", "61616", "663", "5241", "5313", "5524", "7333", "7571", "45141", "52512", "56414"];
+const syncExamples = ["(2x,4x)", "(4,2x)(2x,4)", "(4,4)", "(4,4)(4x,0)(4,4)(0,4x)", "(4,6x)(2x,4)", "(4x,2x)", "(4x,2x)(4,2x)(2x,4x)(2x,4)", "(4x,4x)", "(4x,6)(6,4x)", "(6,4x)(4x,2)", "(6x,2x)", "(6x,2x)(2x,6x)", "(6x,4)(4,2x)(4,6x)(2x,4)", "(6x,4)(4,6x)", "(6x,4x)", "(6x,6x)(2x,2x)", "(2x,2x)", "(8,2x)(4,2x)(2x,8)(2x,4)"];
+const multiplexExamples = ["[54]24", "[43]1421", "4[43]1", "[32]", "[43]23", "[43][32]3", "[31]", "(2,4)([4x4],2x)", "(2,4x)([4x4],2)", "(2,6)([6x6],2x)(6x,2x)"];
 const examples = asyncExamples.concat(syncExamples, multiplexExamples);
 
 
@@ -28,25 +32,25 @@ function mod(n, m) {
 
 function getJuggler(workingHeight) {
     return {
-        headSize: workingHeight*0.15,
-        headY: HEIGHT-workingHeight*0.85,
-        shoulderX: WIDTH*0.5 - workingHeight*0.2,
-        shoulderY: HEIGHT-workingHeight*0.6,
-        hipX: WIDTH*0.5 - workingHeight*0.1,
-        hipY: HEIGHT-workingHeight*0.1,
-        elbowX: WIDTH*0.5 - workingHeight*0.3,
-        elbowY: HEIGHT-workingHeight*0.2,
+        headSize: workingHeight * 0.13,
+        headY: HEIGHT - workingHeight * 0.8,
+        shoulderX: WIDTH * 0.5 - workingHeight * 0.2,
+        shoulderY: HEIGHT - workingHeight * 0.6,
+        hipX: WIDTH * 0.5 - workingHeight * 0.1,
+        hipY: HEIGHT - workingHeight * 0.1,
+        elbowX: WIDTH * 0.5 - workingHeight * 0.3,
+        elbowY: HEIGHT - workingHeight * 0.2,
 
-        ballSize: workingHeight*0.05,
-        crossingTravelDist: workingHeight*0.6,
-        sameTravelDist: workingHeight*0.3,
-        armLength: workingHeight*0.15,
+        ballSize: workingHeight * 0.05,
+        crossingTravelDist: workingHeight * 0.6,
+        sameTravelDist: workingHeight * 0.3,
+        armLength: workingHeight * 0.15,
         height: workingHeight,
         handX: (rot) => {
-            return WIDTH*0.5 - workingHeight*(0.3-0.15*Math.cos(rot));
+            return WIDTH * 0.5 - workingHeight * (0.3 - 0.15 * Math.cos(rot));
         },
         handY: (rot) => {
-            return HEIGHT-workingHeight*(0.2+Math.sin(rot)*0.15);
+            return HEIGHT - workingHeight * (0.2 + Math.sin(rot) * 0.15);
         }
     };
 }
@@ -54,7 +58,7 @@ function getJuggler(workingHeight) {
 function drawPerson(j, leftRotation, rightRotation) {
     // draw head
     ctx.beginPath();
-    ctx.arc(WIDTH*0.5, j.headY, j.headSize, 0, 2 * Math.PI);
+    ctx.arc(WIDTH * 0.5, j.headY, j.headSize, 0, 2 * Math.PI);
     // draw body
     ctx.moveTo(j.shoulderX, j.shoulderY);
     ctx.lineTo(WIDTH - j.shoulderX, j.shoulderY);
@@ -63,11 +67,11 @@ function drawPerson(j, leftRotation, rightRotation) {
     ctx.closePath();
     // draw arms
     ctx.moveTo(j.shoulderX, j.shoulderY);
-    ctx.lineTo(j.elbowX,j.elbowY);
-    ctx.lineTo(j.handX(leftRotation),j.handY(leftRotation));
+    ctx.lineTo(j.elbowX, j.elbowY);
+    ctx.lineTo(j.handX(leftRotation), j.handY(leftRotation));
     ctx.moveTo(WIDTH - j.shoulderX, j.shoulderY);
-    ctx.lineTo(WIDTH - j.elbowX,j.elbowY);
-    ctx.lineTo(WIDTH - j.handX(rightRotation),j.handY(rightRotation));
+    ctx.lineTo(WIDTH - j.elbowX, j.elbowY);
+    ctx.lineTo(WIDTH - j.handX(rightRotation), j.handY(rightRotation));
     ctx.stroke();
 }
 
@@ -75,33 +79,52 @@ function drawBall(juggler, startingHand, ballThrow, progress) {
     ctx.fillStyle = "red";
     ctx.beginPath();
     var x, y, travelDist, beats;
+    let outsideThrow = startingHand === "right" ? RHoutsideThrows : LHoutsideThrows;
+    let otherHandOutside = startingHand === "right" ? LHoutsideThrows : RHoutsideThrows;
     if (ballThrow < 0) { //using negative integers to indicate "forced-crossing" throws
         beats = -ballThrow;
         travelDist = juggler.crossingTravelDist;
+        if (outsideThrow && !otherHandOutside) {
+            travelDist += juggler.sameTravelDist;
+        } else if (!outsideThrow && otherHandOutside) {
+            travelDist -= juggler.sameTravelDist;
+        }
     } else {
         beats = ballThrow;
-        if (beats%2 == 1){
+        if (beats % 2 == 1) {
             travelDist = juggler.crossingTravelDist;
+            if (outsideThrow && !otherHandOutside) {
+                travelDist += juggler.sameTravelDist;
+            } else if (!outsideThrow && otherHandOutside) {
+                travelDist -= juggler.sameTravelDist;
+            }
         } else {
-            travelDist = -juggler.sameTravelDist;
+            travelDist = outsideThrow ? juggler.sameTravelDist : -juggler.sameTravelDist;
         }
     }
-
     if (progress <= 0.5) {
-        x = juggler.handX(progress*2 * Math.PI - Math.PI);
-        y = juggler.handY(progress*2 * Math.PI - Math.PI);
-    } else {
-        x = juggler.elbowX + juggler.armLength + (progress - 0.5)/(beats-0.5)*(travelDist);
-        y = juggler.elbowY + juggler.height*(gravity*beatLength)*(progress - 0.5)*(progress-beats);
+        let ang = flipAngle(progress * 2 * Math.PI - Math.PI, outsideThrow)
+        x = juggler.handX(ang);
+        y = juggler.handY(ang);
+    }
+    else if (showTwosAsHolds && ballThrow == 2) {
+        let ang = flipAngle((progress - 0.5) * 2 * Math.PI / 3, outsideThrow)
+        x = juggler.handX(ang);
+        y = juggler.handY(ang);
+    }
+    else {
+        let outsideThrowShift = outsideThrow ? 2 * juggler.armLength : 0;
+        x = juggler.elbowX + juggler.armLength - outsideThrowShift + (progress - 0.5) / (beats - 0.5) * (travelDist);
+        y = juggler.elbowY + juggler.height * (gravity * beatLength) * (progress - 0.5) * (progress - beats);
     }
     if (startingHand === "right") {
-        x = WIDTH-x;
+        x = WIDTH - x;
     }
-    ctx.arc(x,y,juggler.ballSize, 0, 2 * Math.PI);
+    ctx.arc(x, y, juggler.ballSize, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-function checkThrow(ballThrow, multiplexAllowed, sync) { 
+function checkThrow(ballThrow, multiplexAllowed, sync) {
     // checks if a string is a valid representation of a single-handed throw. 
     // returns an empty array if invalid
     if (ballThrow[0] == "[") { //multiplex
@@ -109,10 +132,10 @@ function checkThrow(ballThrow, multiplexAllowed, sync) {
             return [];
         } else {
             let throwsList = [];
-            for (let i = 1; i < ballThrow.length-1; i++) { //start at 1 and end at length-1 to ignore the square brackets
+            for (let i = 1; i < ballThrow.length - 1; i++) { //start at 1 and end at length-1 to ignore the square brackets
                 let res;
-                if (ballThrow[i+1] == "x") {
-                    res = checkThrow(ballThrow.slice(i,i+2), false, sync)[0];
+                if (ballThrow[i + 1] == "x") {
+                    res = checkThrow(ballThrow.slice(i, i + 2), false, sync)[0];
                     i++; //skip the "x"
                 } else {
                     res = checkThrow(ballThrow[i], false, sync)[0];
@@ -128,15 +151,15 @@ function checkThrow(ballThrow, multiplexAllowed, sync) {
     } else { //normal throw
         if (ballThrow.length == 1) {
             let index = alphabet.indexOf(ballThrow);
-            if (index != -1 && (!sync || index%2 == 0)) { //sync throws must be an even number of beats);
+            if (index != -1 && (!sync || index % 2 == 0)) { //sync throws must be an even number of beats);
                 return [index];
             }
-        } else if (sync && ballThrow.length == 2 && ballThrow[1] == "x" && alphabet.includes(ballThrow[0]) ) {
+        } else if (sync && ballThrow.length == 2 && ballThrow[1] == "x" && alphabet.includes(ballThrow[0])) {
             let index = alphabet.indexOf(ballThrow[0]);
-            if (index%2 == 0 && index > 0) { //crossing sync throws must be an even nonzero number of beats
-                return [-1*index]; // negative numbers are used to indicate crossing throws
+            if (index % 2 == 0 && index > 0) { //crossing sync throws must be an even nonzero number of beats
+                return [-1 * index]; // negative numbers are used to indicate crossing throws
             }
-        } 
+        }
         return [];
     }
 }
@@ -149,7 +172,7 @@ function parseSiteswap(siteswap) { //returns a list of all the throws, or an emp
             if (!pairs[i].endsWith(")")) {
                 return [];
             }
-            let splitPair = pairs[i].slice(0,-1).split(",")
+            let splitPair = pairs[i].slice(0, -1).split(",")
             if (splitPair.length != 2) {
                 return [];
             } else {
@@ -173,8 +196,8 @@ function parseSiteswap(siteswap) { //returns a list of all the throws, or an emp
         for (let i = 0; i < siteswap.length; i++) {
             let ballThrow = siteswap[i];
             if (ballThrow === "[") { //dealing with a multiplex
-                let closingIndex = siteswap.indexOf("]",i);
-                ballThrow = siteswap.slice(i,closingIndex+1);
+                let closingIndex = siteswap.indexOf("]", i);
+                ballThrow = siteswap.slice(i, closingIndex + 1);
                 i = closingIndex;
             }
             let parsedThrow = checkThrow(ballThrow, true, false);
@@ -193,17 +216,17 @@ function parseSiteswap(siteswap) { //returns a list of all the throws, or an emp
     }
 }
 
-function checkSiteswap(siteswap, sync) { 
+function checkSiteswap(siteswap, sync) {
     let beats = siteswap.length;
-    if (sync && beats%2 == 1) throw "Error: somehow sync siteswap has odd number of throws!";
+    if (sync && beats % 2 == 1) throw "Error: somehow sync siteswap has odd number of throws!";
     let catchesEachBeat = Array(beats).fill(0);
     for (let i = 0; i < beats; i++) {
         siteswap[i].forEach(ball => {
             if (ball < 0) {
-                let adjustment = -2*(i%2) + 1;
-                catchesEachBeat[mod(i-ball+adjustment,beats)] += 1; //subtracting ball since it is stored as a negative number
+                let adjustment = -2 * (i % 2) + 1;
+                catchesEachBeat[mod(i - ball + adjustment, beats)] += 1; //subtracting ball since it is stored as a negative number
             } else {
-                catchesEachBeat[(i+ball)%beats] += 1;
+                catchesEachBeat[(i + ball) % beats] += 1;
             }
         });
     }
@@ -223,14 +246,14 @@ function getMaxThrow(siteswap) {
 
 function calcIdealWorkingHeight(siteswap) {
     let maxThrow = getMaxThrow(siteswap);
-    return Math.min(590,(10-HEIGHT)/(-0.25 + (gravity*beatLength)*((maxThrow-0.5)/2)*(-maxThrow/2 + 0.25)));
+    return Math.min(590, (10 - HEIGHT) / (-0.25 + (gravity * beatLength) * ((maxThrow - 0.5) / 2) * (-maxThrow / 2 + 0.25)));
 }
 
 function showInvalidSiteSwap() {
-    ctx.clearRect(0,0,WIDTH,HEIGHT)
+    ctx.clearRect(0, 0, WIDTH, HEIGHT)
     ctx.fillStyle = "black";
     ctx.font = "30px Arial";
-    ctx.fillText("Invalid Siteswap", WIDTH*0.5 - ctx.measureText("Invalid Siteswap").width*0.5, HEIGHT*0.5);
+    ctx.fillText("Invalid Siteswap", WIDTH * 0.5 - ctx.measureText("Invalid Siteswap").width * 0.5, HEIGHT * 0.5);
 }
 
 siteswapButton.onclick = () => {
@@ -257,59 +280,75 @@ beatSlider.onchange = () => {
 beatSlider.onchange();
 gravitySlider.onchange = () => {
     let newGravity = gravitySlider.value * 0.0002;
-    document.getElementById("gravity-text").textContent = "Gravity: " + (newGravity*1000).toPrecision(2);
+    document.getElementById("gravity-text").textContent = "Gravity: " + (newGravity * 1000).toPrecision(2);
     gravity = newGravity;
     siteswapButton.onclick();
 };
 gravitySlider.onchange();
 
+showTwosCheckbox.onchange = () => {
+    showTwosAsHolds = !showTwosAsHolds;
+};
+
+document.getElementById("lh-outside").onchange = () => {
+    LHoutsideThrows = !LHoutsideThrows;
+};
+document.getElementById("rh-outside").onchange = () => {
+    RHoutsideThrows = !RHoutsideThrows;
+};
+
+
 document.getElementById("example-button").onclick = () => {
-    document.getElementById("siteswap-input").value = examples[Math.floor(Math.random()*examples.length)]
+    document.getElementById("siteswap-input").value = examples[Math.floor(Math.random() * examples.length)]
     siteswapButton.onclick();
 };
 
-function getRotation(beats,start) {
-    if ((beats+start)%2 < 1.5) {
-        return 2*(((beats+start)%2))*Math.PI/3;
+function getRotation(beats, start) {
+    if ((beats + start) % 2 < 1.5) {
+        return 2 * (((beats + start) % 2)) * Math.PI / 3;
     } else {
-        return 2*(((beats+start)%2))*Math.PI;
+        return 2 * (((beats + start) % 2)) * Math.PI;
     }
+}
+
+function flipAngle(angle, flip) {
+    return flip ? Math.PI - angle : angle;
 }
 
 function drawAsyncSiteswap(juggler, siteswap, beats) {
     let maxThrowSize = getMaxThrow(siteswap); //amount of beats we need to backtrack
     for (let i = 0; i < maxThrowSize; i++) {
         let hand, throwHeights, progress;
-        if ((Math.floor(beats)-i)%2 != 0) {
+        if ((Math.floor(beats) - i) % 2 != 0) {
             hand = "left";
         } else {
             hand = "right";
         }
-        progress = beats%1 + i;
-        throwHeights = siteswap[mod(Math.floor(beats)-i-1,siteswap.length)];
+        progress = beats % 1 + i;
+        throwHeights = siteswap[mod(Math.floor(beats) - i - 1, siteswap.length)];
         throwHeights.forEach((height) => {
             if (progress <= height) {
-                drawBall(juggler, hand, height, progress);					
+                drawBall(juggler, hand, height, progress);
             }
         });
     }
 }
 
 function drawSyncSiteswap(juggler, siteswap, beats) {
-    if (siteswap.length%2 != 0) throw "Error: somehow sync siteswap has odd number of throws!";;
+    if (siteswap.length % 2 != 0) throw "Error: somehow sync siteswap has odd number of throws!";;
     let maxThrowSize = getMaxThrow(siteswap); //amount of beats we need to backtrack
     for (let i = 0; i < maxThrowSize; i += 2) {
-        let progress = beats%2 + i;
-        let leftThrowHeights = siteswap[mod(Math.floor(beats*0.5)*2-i-1,siteswap.length)];
-        let rightThrowHeights = siteswap[mod(Math.floor(beats*0.5)*2-i-2,siteswap.length)];
+        let progress = beats % 2 + i;
+        let leftThrowHeights = siteswap[mod(Math.floor(beats * 0.5) * 2 - i - 1, siteswap.length)];
+        let rightThrowHeights = siteswap[mod(Math.floor(beats * 0.5) * 2 - i - 2, siteswap.length)];
         leftThrowHeights.forEach((height) => {
             if (progress <= Math.abs(height)) {
-                drawBall(juggler, "left", height, progress);					
+                drawBall(juggler, "left", height, progress);
             }
         });
         rightThrowHeights.forEach((height) => {
             if (progress <= Math.abs(height)) {
-                drawBall(juggler, "right", height, progress);					
+                drawBall(juggler, "right", height, progress);
             }
         });
     }
@@ -317,18 +356,18 @@ function drawSyncSiteswap(juggler, siteswap, beats) {
 
 function doStuff(timeStamp) {
     if (animationOngoing) {
-        ctx.clearRect(0,0,WIDTH,HEIGHT)
-        let currentTime = timeStamp-startingTime;
-        let beats = currentTime/beatLength;
+        ctx.clearRect(0, 0, WIDTH, HEIGHT)
+        let currentTime = timeStamp - startingTime;
+        let beats = currentTime / beatLength;
         let leftRotation, rightRotation;
         if (globalSync) {
-            leftRotation = getRotation(beats,1.5);
-            rightRotation = getRotation(beats,1.5);
+            leftRotation = flipAngle(getRotation(beats, 1.5), LHoutsideThrows);
+            rightRotation = flipAngle(getRotation(beats, 1.5), RHoutsideThrows);
         } else {
-            leftRotation = getRotation(beats,0.5);
-            rightRotation = getRotation(beats,1.5);
+            leftRotation = flipAngle(getRotation(beats, 0.5), LHoutsideThrows);
+            rightRotation = flipAngle(getRotation(beats, 1.5), RHoutsideThrows);
         }
-        drawPerson(globalJuggler,leftRotation,rightRotation);
+        drawPerson(globalJuggler, leftRotation, rightRotation);
         if (globalSync) {
             drawSyncSiteswap(globalJuggler, globalSiteswap, beats)
         } else {
